1ecfd93ee474a1fe89b4257c0fefc51a3680b560
zefonsecasj <csjuniod@gmail.com>
2023-02-05 22:58:04 -0300

    Some few changes, but nothing too important

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 var counter = 1
 
+//Functions to format the currency string
+var currency = 'BRL'
+var currencyInput = document.querySelector('input[type=currency]')
+
+onBlur({target:currencyInput})
+currencyInput.addEventListener('focus', onFocus)
+currencyInput.addEventListener('blur', onBlur)
+function onFocus(e){
+    var value = e.target.value;
+    e.target.value = value ? localStringToNumber(value) : ''
+}
+function onBlur(e){
+    var value = e.target.value;
+    var options = {
+        maximumFractionDigits : 2,
+        currency : currency,
+        style : 'currency',
+        currencyDisplay : "symbol"
+    }
+    e.target.value = (value || value === 0) ? localStringToNumber(value).toLocaleString(undefined,  options) : ''
+}
+
+function localStringToNumber(text){
+    return Number(String(text).replace(/[^0-9.-]+/g,""))
+}
+
 function add_item(){
 
     var itemName = window.document.getElementById('Item')
@@ -41,3 +67,11 @@ function edit_table(){
     window.alert('test')
 }
 
+
+function output_graph(){
+    var reportDiv = document.getElementById('report_div')
+    var labels = ["Food", "Cleaning", "Leisure", "Gas", "Others"]
+    var table = window.document.getElementById('mainTable')
+    var tableRowsLenght = table.rows.length;
+
+}
